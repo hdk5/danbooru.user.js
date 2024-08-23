48a10d7117010cca2f84550bbe9e1723e087f023
hdk5 <hdk5@hdk5.xyz>
2024-08-23 16:52:34 +0300

    u-t-d.bd: add custom booru setting

--- dist/upload-to-danbooru.bd.plugin.js
+++ dist/upload-to-danbooru.bd.plugin.js
@@ -2,18 +2,25 @@
  * @name UploadToDanbooru
  * @author hdk5
  * @authorId 219173750980083712
- * @version 20240822000840
+ * @version 20240823135202
  * @description Quickly upload media to danbooru
  * @source https://github.com/hdk5/danbooru.user.js
  * @updateUrl https://github.com/hdk5/danbooru.user.js/raw/master/dist/upload-to-danbooru.bd.plugin.js
  */
 
+const DEFAULT_BOORU = 'https://danbooru.donmai.us/'
+
 module.exports = class UploadToDanbooru {
   constructor(meta) {
     this.meta = meta
+    this.settings = {
+      booru: '',
+    }
   }
 
   start() {
+    Object.assign(this.settings, BdApi.Data.load(this.meta.name, 'settings'))
+
     this.unpatchMessageContext = BdApi.ContextMenu.patch('message', (menu, props) => {
       if (props.mediaItem === undefined)
         return
@@ -26,8 +33,7 @@ module.exports = class UploadToDanbooru {
       if (copyLinkParent.some(button => button?.props?.id === this.meta.slug))
         return
 
-      // TODO: custom booru settings
-      const uploadUrl = new URL('uploads/new', 'http://localhost:10115/')
+      const uploadUrl = new URL('uploads/new', this.settings.booru || DEFAULT_BOORU)
       uploadUrl.searchParams.set('url', props.mediaItem.url)
       uploadUrl.searchParams.set('ref', `https://discord.com/channels/${props.channel.guild_id}/${props.message.channel_id}/${props.message.id}`)
 
@@ -44,5 +50,39 @@ module.exports = class UploadToDanbooru {
   stop() {
     BdApi.Patcher.unpatchAll(this.meta.slug)
     this.unpatchMessageContext()
+  }
+
+  getSettingsPanel() {
+    const settingsPanel = Object.assign(document.createElement('div'), {
+      id: 'utd-settings',
+    })
+    const booruSettingsItem = Object.assign(document.createElement('div'), {
+      className: 'bd-setting-item',
+    })
+    const booruSettingsHeader = Object.assign(document.createElement('div'), {
+      className: 'bd-setting-header',
+    })
+    const booruSettingsLabel = Object.assign(document.createElement('label'), {
+      for: 'utd-settings-booru',
+      className: 'bd-setting-title',
+      textContent: 'Danbooru URL',
+    })
+    const booruSettingsInput = Object.assign(document.createElement('input'), {
+      type: 'text',
+      name: 'utd-settings-booru',
+      id: 'utd-settings-booru',
+      value: this.settings.booru,
+      placeholder: DEFAULT_BOORU,
+    })
+    booruSettingsInput.addEventListener('change', (e) => {
+      this.settings.booru = e.target.value
+      BdApi.Data.save(this.meta.name, 'settings', this.settings)
+    })
+    settingsPanel.append(booruSettingsItem)
+    booruSettingsItem.append(booruSettingsHeader)
+    booruSettingsHeader.append(booruSettingsLabel)
+    booruSettingsItem.append(booruSettingsInput)
+
+    return settingsPanel
   }
 }
