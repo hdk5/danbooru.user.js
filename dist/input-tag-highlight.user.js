566b23202089f7b7f66e04e4e17001fa8e6e91bd
hdk5 <hdk5@hdk5.xyz>
2025-12-24 08:24:37 +0200

    i-t-h: lint

--- dist/input-tag-highlight.user.js
+++ dist/input-tag-highlight.user.js
@@ -117,7 +117,7 @@ const SCRIPT_CSS = /* CSS */`
       --tag-highlight-meta-value-color: var(--grey-2);
     }
   }
-`
+`;
 
 const RECLASS_METATAGS = {
   ch: 4,
@@ -131,7 +131,7 @@ const RECLASS_METATAGS = {
   character: 4,
   copyright: 3,
   artist: 1,
-}
+};
 const METATAGS = [
   ...Object.keys(RECLASS_METATAGS),
   'parent',
@@ -152,60 +152,64 @@ const METATAGS = [
   'disapproved',
   'status',
   '-status',
-]
-const TAG_CACHE = {}
+];
+const TAG_CACHE = {};
 
 // cba to set up the bundler to use my proper parser tbh
 function tokenize(input) {
-  const tokens = []
+  const tokens = [];
 
-  let i = 0
+  let i = 0;
   while (i < input.length) {
-    const char = input[i]
+    const char = input[i];
 
     if (/\s/.test(char)) {
-      tokens.push({ type: 'whitespace', value: char })
-      i++
-      continue
+      tokens.push({ type: 'whitespace', value: char });
+      i++;
+      continue;
     }
 
-    const metaName = METATAGS.find(name => input.startsWith(`${name}:`, i))
+    const metaName = METATAGS.find(name => input.startsWith(`${name}:`, i));
     if (metaName !== undefined) {
-      i += metaName.length + 1
+      i += metaName.length + 1;
 
-      let value = ''
+      let value = '';
 
       if (input[i] === '"' || input[i] === '\'') {
-        value += input[i++]
+        value += input[i++];
 
         while (i < input.length) {
-          value += input[i++]
-          if (input[i - 1] === value[0] && input[i - 2] !== '\\')
-            break
+          value += input[i++];
+          if (input[i - 1] === value[0] && input[i - 2] !== '\\') {
+            break;
+          }
         }
       }
       else {
-        while (i < input.length && !/\s/.test(input[i]))
-          value += input[i++]
+        while (i < input.length && !/\s/.test(input[i])) {
+          value += input[i++];
+        }
       }
 
-      tokens.push({ type: 'metatag', name: metaName, value })
-      continue
+      tokens.push({ type: 'metatag', name: metaName, value });
+      continue;
     }
 
-    let tag = ''
-    while (i < input.length && !/\s/.test(input[i]))
-      tag += input[i++]
+    let tag = '';
+    while (i < input.length && !/\s/.test(input[i])) {
+      tag += input[i++];
+    }
 
-    if (tag)
-      tokens.push({ type: 'tag', value: tag })
+    if (tag) {
+      tokens.push({ type: 'tag', value: tag });
+    }
   }
 
-  return tokens
+  return tokens;
 }
 
 function normalizeTag(tag) {
-  return tag.toLowerCase()
+  return tag.toLowerCase();
 }
 
 async function fillTagCache(tokens) {
@@ -213,11 +217,11 @@ async function fillTagCache(tokens) {
   // https://github.com/danbooru/danbooru/issues/5850
   const missingTags = tokens
     .filter(token => token.type === 'tag' && !(token.value in TAG_CACHE))
-    .map(token => normalizeTag(token.value))
+    .map(token => normalizeTag(token.value));
 
-  const chunkSize = 1000
+  const chunkSize = 1000;
   for (let i = 0; i < missingTags.length; i += chunkSize) {
-    const chunk = missingTags.slice(i, i + chunkSize)
+    const chunk = missingTags.slice(i, i + chunkSize);
     const resp = await $.post('/tags.json', {
       _method: 'get',
       limit: chunkSize,
@@ -225,120 +229,126 @@ async function fillTagCache(tokens) {
       search: {
         name_array: chunk,
       },
-    })
-    const tagData = Object.fromEntries(resp.map(tag => [tag.name, tag]))
+    });
+    const tagData = Object.fromEntries(resp.map(tag => [tag.name, tag]));
     for (const tagName of chunk) {
       const tag = tagData[tagName] ?? {
         name: tagName,
         post_count: 0,
         category: 0,
         is_deprecated: false,
-      }
-      TAG_CACHE[tagName] = tag
+      };
+      TAG_CACHE[tagName] = tag;
     }
   }
 
-  return missingTags.length
+  return missingTags.length;
 }
 
 function applyHighlights(tokens) {
-  const nodes = []
+  const nodes = [];
 
   for (const token of tokens) {
-    const htmlToken = $('<span></span>')
+    const htmlToken = $('<span></span>');
     if (token.type === 'metatag') {
-      const htmlName = $('<span></span>')
-      htmlName.addClass('tag-highlight-meta-name')
-      htmlName.text(`${token.name}:`)
+      const htmlName = $('<span></span>');
+      htmlName.addClass('tag-highlight-meta-name');
+      htmlName.text(`${token.name}:`);
 
-      const htmlValue = $('<span></span>')
-      const cat = RECLASS_METATAGS[token.name]
-      if (cat !== undefined)
-        htmlToken.addClass(`tag-highlight-type-${cat}`)
-      else
-        htmlValue.addClass('tag-highlight-meta-value')
-      htmlValue.text(token.value)
+      const htmlValue = $('<span></span>');
+      const cat = RECLASS_METATAGS[token.name];
+      if (cat !== undefined) {
+        htmlToken.addClass(`tag-highlight-type-${cat}`);
+      }
+      else {
+        htmlValue.addClass('tag-highlight-meta-value');
+      }
+      htmlValue.text(token.value);
 
-      htmlToken.html([htmlName, htmlValue])
+      htmlToken.html([htmlName, htmlValue]);
     }
     else if (token.type === 'tag') {
-      htmlToken.text(token.value)
+      htmlToken.text(token.value);
       const tagData = TAG_CACHE[normalizeTag(token.value)] ?? {
         category: 0,
         post_count: 0,
         is_deprecated: false,
+      };
+      if (tagData.is_deprecated) {
+        htmlToken.addClass('tag-highlight-deprecated');
       }
-      if (tagData.is_deprecated)
-        htmlToken.addClass('tag-highlight-deprecated')
-      else if (!tagData.post_count)
-        htmlToken.addClass('tag-highlight-empty')
+      else if (!tagData.post_count) {
+        htmlToken.addClass('tag-highlight-empty');
+      }
 
-      htmlToken.addClass(`tag-highlight-type-${tagData.category}`)
+      htmlToken.addClass(`tag-highlight-type-${tagData.category}`);
     }
     else if (token.type === 'whitespace') {
-      htmlToken.text(token.value)
+      htmlToken.text(token.value);
     }
 
-    nodes.push(htmlToken)
+    nodes.push(htmlToken);
   }
 
-  return nodes
+  return nodes;
 }
 
 $('#post_tag_string').each((i, el) => {
-  GM_addStyle(SCRIPT_CSS)
+  GM_addStyle(SCRIPT_CSS);
 
-  const $input_textarea = $(el)
+  const $input_textarea = $(el);
   const $input_container = $('<div></div>', {
     class: 'tag-highlight-container',
-  })
+  });
   const $input_backdrop = $('<div></div>', {
     class: 'tag-highlight-backdrop',
     inert: '',
-  })
+  });
   const $input_highlights = $('<div></div>', {
     class: 'tag-highlight-highlights',
-  })
-  $input_container.append($input_backdrop)
-  $input_backdrop.append($input_highlights)
+  });
+  $input_container.append($input_backdrop);
+  $input_backdrop.append($input_highlights);
 
   $input_textarea.on({
     'input': handleInput,
     'focus': handleInput,
     'danbooru:update-tag-counter': handleInput,
     'scroll': handleScroll,
-  })
+  });
 
-  let handleInputReq
+  let handleInputReq;
   async function handleInput() {
-    const currentReq = {}
-    handleInputReq = currentReq
+    const currentReq = {};
+    handleInputReq = currentReq;
 
-    const text = $input_textarea.val()
-    const tokens = tokenize(text)
+    const text = $input_textarea.val();
+    const tokens = tokenize(text);
 
-    $input_highlights.html(applyHighlights(tokens))
-    handleScroll()
+    $input_highlights.html(applyHighlights(tokens));
+    handleScroll();
 
-    const tagsLoaded = await fillTagCache(tokens)
+    const tagsLoaded = await fillTagCache(tokens);
 
-    if (tagsLoaded === 0 || handleInputReq !== currentReq)
-      return
+    if (tagsLoaded === 0 || handleInputReq !== currentReq) {
+      return;
+    }
 
-    $input_highlights.html(applyHighlights(tokens))
+    $input_highlights.html(applyHighlights(tokens));
   }
 
   function handleScroll() {
-    const scrollTop = $input_textarea.scrollTop()
-    $input_backdrop.scrollTop(scrollTop)
+    const scrollTop = $input_textarea.scrollTop();
+    $input_backdrop.scrollTop(scrollTop);
 
-    const scrollLeft = $input_textarea.scrollLeft()
-    $input_backdrop.scrollLeft(scrollLeft)
+    const scrollLeft = $input_textarea.scrollLeft();
+    $input_backdrop.scrollLeft(scrollLeft);
   }
 
-  $input_textarea.after($input_container)
-  $input_container.append($input_textarea.detach())
+  $input_textarea.after($input_container);
+  $input_container.append($input_textarea.detach());
 
-  if ($input_textarea.is(':visible'))
-    handleInput()
-})
+  if ($input_textarea.is(':visible')) {
+    handleInput();
+  }
+});
